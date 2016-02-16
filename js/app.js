76eaba6c6b3b954dc7233db6afca48636e7896f3
snilssonboy <simon.nilsson@dohi.se>
2016-02-16 15:34:52 +0100

    Second commit

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ console.log("prutt");
 
 
 function GetData(){
-	$.get("http://snilssonboy.github.io/simonmarkus/?url=http://www.dotabuff.com/players/48675258/matches", function(response) { alert(response) });
+	$.get("http://snilssonboy.github.io/dotalyzer/?url=http://www.dotabuff.com/players/48675258/matches", function(response) { alert(response) });
 }
 
 console.log("test1337");
