3830f870b0fc01eed437ec6d64a2f41cc43a0b69
snilssonboy <simon.nilsson@dohi.se>
2016-02-16 15:32:55 +0100

    Adding files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log("prutt");
+
+
+function GetData(){
+	$.get("http://snilssonboy.github.io/simonmarkus/?url=http://www.dotabuff.com/players/48675258/matches", function(response) { alert(response) });
+}
+
+console.log("test1337");
